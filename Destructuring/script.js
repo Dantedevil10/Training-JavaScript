250d28a14ce914e7aabb5b6bbd6fc0977f26a83b
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-03-16 08:57:20 -0300

    Add files via upload

---
+++ Destructuring/script.js
@@ -0,0 +1,11 @@
+let Characters = [{
+    firstname: "Spongebob",
+    lastname: "Squarepants",
+    age: 30
+}]
+
+
+const [{firstname,lastname,age}] = Characters
+
+
+console.log(firstname,lastname,age)
